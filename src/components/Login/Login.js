2e1b2e85a7eec24147832f56b68a827eebb356b9
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-06 12:47:41 +0100

    feat: Adding Timeout so validity check is done after a specified time and not every key stroke, which would cause unecessary network traffic

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );    
+    const timeoutHandler = setTimeout(()=>{
+      console.log('checking validity');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      ); 
+    }, 500);
+
+    return () => {
+      console.log('Timeout Clean up')
+      clearTimeout(timeoutHandler);
+    };
+   
   },[enteredEmail,enteredPassword]);
 
 
